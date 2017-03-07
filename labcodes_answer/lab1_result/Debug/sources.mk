b016e415b633c23a19bbb89859004436d54c04f5
lihy96 <lhy237451594@vip.qq.com>
2017-03-06 23:58:42 -0800

    lab1 exer5

---
+++ labcodes_answer/lab1_result/Debug/sources.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+O_SRCS := 
+S_UPPER_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+tools \
+obj/sign/tools \
+obj/libs \
+obj \
+obj/kern/trap \
+obj/kern/mm \
+obj/kern/libs \
+obj/kern/init \
+obj/kern/driver \
+obj/kern/debug \
+obj/boot \
+libs \
+kern/trap \
+kern/mm \
+kern/libs \
+kern/init \
+kern/driver \
+kern/debug \
+boot \
+
